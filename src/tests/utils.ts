aaa84fc257d9186b878db95063c15c06cb9e1034
Matheus Cristian <mathues@email.com>
2024-09-26 13:02:06 -0300

    test(setup): adds testing setup

---
+++ src/tests/utils.ts
@@ -0,0 +1,32 @@
+import { UnnnicSystem } from '@/plugins/UnnnicSystem';
+import { createTestingPinia } from '@pinia/testing';
+import { mount } from '@vue/test-utils';
+import { vi } from 'vitest';
+
+export const setup = (...args: Parameters<typeof mount>) => {
+  const [component, options] = args;
+
+  return mount(component, {
+    props: options?.props,
+
+    global: {
+      mocks: {
+        $t: (key: string) => key,
+        ...(options?.global?.mocks || {}),
+      },
+
+      stubs: {
+        I18nT: { template: '<span><slot/></span>' },
+        ...options?.global?.stubs,
+      },
+
+      plugins: [
+        UnnnicSystem,
+        createTestingPinia({
+          createSpy: vi.fn,
+        }),
+        ...(options?.global?.plugins || []),
+      ],
+    },
+  });
+};
