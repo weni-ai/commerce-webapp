df9a67af318c58e1e06a045c760c7e22d37b70e2
acnormun <anaclara.noronha@weni.ai>
2024-10-09 17:39:17 -0300

    fix: add img to modal integrate

--- src/components/solutions/ModalIntegrate.vue
+++ src/components/solutions/ModalIntegrate.vue
@@ -1,11 +1,7 @@
 <template>
   <UnnnicModalDialog
-    v-model="modelValue"
     class="modal-integrate-solution"
     :showCloseIcon="true"
-    :hideSecondaryButton="true"
-    :primaryButtonProps="buttonProps"
-    @primary-button-click="emitValue"
   >
     <section class="modal-integrate-solution__container">
       <section class="modal-integrate-solution__body">
@@ -13,10 +9,20 @@
           {{ description }}
         </p>
 
+        <UnnnicButton
+          class="modal-integrate-solution__integrate-button"
+          size="small"
+          @click="
+            $emit('close');
+            $emit('integrate');
+          "
+        >
+          {{ $t('solutions.integrate.button_label') }}
+        </UnnnicButton>
+
         <section
           v-if="!!tip"
           class="modal-integrate-solution__tip"
-          data-test="tip-box"
         >
           <UnnnicIcon
             icon="emoji_objects"
@@ -27,74 +33,62 @@
           {{ tip }}
         </section>
       </section>
+
+      <img
+        class="modal-integrate-solution__preview-image"
+        :src="getImageByUuid(uuid)"
+        alt="Chat"
+      />
     </section>
   </UnnnicModalDialog>
 </template>
 
 <script lang="ts" setup>
-import { useI18n } from 'vue-i18n';
-
-const modelValue = defineModel<boolean>({ required: true });
-const { t } = useI18n();
-
-const props = defineProps<{
+defineProps<{
   description: string;
   tip?: string;
-  status: 'available' | 'integrated';
+  uuid: string;
 }>();
 
-const emit = defineEmits<{
+defineEmits<{
+  close: [];
   integrate: [];
-  edit: [];
 }>();
 
-const buttonProps =
-  props.status === 'available'
-    ? {
-        class: 'modal-integrate-solution__integrate-button',
-        text: t('solutions.integrate.button_label'),
-        size: 'large',
-        'data-test': 'integrate-button',
-      }
-    : props.status === 'integrated'
-      ? {
-          class: 'modal-integrate-solution__edit-button',
-          text: t('solutions.details.view_settings'),
-          size: 'large',
-          'data-test': 'edit-button',
-        }
-      : {};
+const images: Record<string, string> = {
+  '4d983f31-065b-45c8-84fd-276469750c38': abandoned_cart,
+  'adc0315c-6549-4321-93c6-0557b55a6ca2': status,
+  '6e38bfb1-a0e1-4dc7-95b3-b14ebc94d1a5': status_passive,
+};
 
-function emitValue() {
-  modelValue.value = false;
-  if (props.status === 'available') {
-    emit('integrate');
-  } else if (props.status === 'integrated') {
-    emit('edit');
-  }
+function getImageByUuid(uuid: string): string {
+  return images[uuid] || abandoned_cart;
 }
+
+import abandoned_cart from '@/assets/abandoned_cart.png';
+import status from '@/assets/status.png';
+import status_passive from '@/assets/status_passive.png';
 </script>
 
 <style scoped lang="scss">
 .modal-integrate-solution {
   :deep(.unnnic-modal-dialog__container) {
-    width: 25 * $unnnic-font-size;
+    width: 43.75 * $unnnic-font-size;
     border-radius: $unnnic-border-radius-lg;
+  }
+
+  :deep(.unnnic-modal-dialog__container__header) {
+    padding: $unnnic-spacing-lg;
   }
 
   :deep(.unnnic-modal-dialog__container__content) {
     padding-inline: $unnnic-spacing-lg;
     padding-bottom: $unnnic-spacing-lg;
-    border-bottom: solid 1px $unnnic-color-neutral-soft;
-
-    button {
-      width: 100%;
-    }
   }
 
   :deep(.unnnic-modal-dialog__container__header) {
-    padding: $unnnic-spacing-md;
-    border-bottom-width: 1px;
+    padding-bottom: 0;
+    border-bottom-width: 0;
 
     .unnnic-modal-dialog__container__title-text {
       font-family: $unnnic-font-family-secondary;
@@ -104,22 +98,9 @@ function emitValue() {
     }
   }
 
-  :deep(.unnnic-modal-dialog__container__actions) {
-    display: flex;
-
-    button {
-      width: 100%;
-    }
-  }
-
   &__container {
     display: flex;
-    flex-direction: column;
     column-gap: $unnnic-spacing-giant;
-  }
-
-  &__buttons {
-    padding-top: $unnnic-spacing-md;
   }
 
   &__body {
