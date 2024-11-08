a81198c436107bbfe4538cd1f4571ec992fcad50
Matheus Cristian <mathues@email.com>
2024-11-08 12:04:16 -0300

    feat(utils): removes unused isSolutionIntegrated util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,5 @@
-import { useSolutionsStore } from '@/stores/Solutions';
 import type { z } from 'zod';
 import * as Sentry from '@sentry/vue';
-
-export function isSolutionIntegrated({ uuid }: Pick<Solution, 'uuid'>) {
-  const solutionsStore = useSolutionsStore();
-
-  const integrated = [
-    solutionsStore.integrated.activeNotifications.data,
-    solutionsStore.integrated.passiveService.data,
-  ].flat();
-
-  return integrated.some((integrated) => integrated.uuid === uuid);
-}
 
 export function checkZodScheme(scheme: z.AnyZodObject, data: unknown) {
   const { error } = scheme.safeParse(data);
