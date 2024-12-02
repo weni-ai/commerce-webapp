b1170b65bfd0394812122f89ef1368ceecd70e10
Marcus Vinícius <marcusvinici25@gmail.com>
2024-12-02 09:48:07 -0300

    adjusting getEnv utils

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,6 @@
 function getEnv(name: string): string | undefined {
   return (
-    window?.configs?.[`VITE_APP_${name}`] ||
-    window?.configs?.[name] ||
-    import.meta.env[`VITE_APP_${name}`] ||
-    import.meta.env[name]
+    process.env?.[name] || window?.configs?.[name] || import.meta.env[name]
   );
 }
 
