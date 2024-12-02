ee7ab6fb527bbf533216d1cd6be995964a798c74
Marcus Vinícius <marcusvinici25@gmail.com>
2024-12-02 11:24:41 -0300

    adjusting rspack

--- rspack.config.js
+++ rspack.config.js
@@ -3,6 +3,7 @@ import { rspack } from '@rspack/core';
 import { VueLoaderPlugin } from 'vue-loader';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
+import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,13 +16,28 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   context: __dirname,
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+      serveIndex: true,
+      watch: true,
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    clean: true,
+  },
   entry: {
     main: './src/main.ts',
   },
   resolve: {
     extensions: ['...', '.ts', '.vue'],
     alias: {
-      '@': resolve(__dirname, 'src'), // Alias para a pasta `src`
+      '@': resolve(__dirname, 'src'),
     },
   },
   module: {
@@ -51,18 +67,18 @@ export default defineConfig({
         ],
       },
       {
-        test: /\.(scss|sass)$/, // Para suportar ambas as extensões .scss e .sass
-        loader: 'sass-loader', // Usa o sass-loader
-        type: 'css', // Configura o processamento como CSS
+        test: /\.(scss|sass)$/,
+        loader: 'sass-loader',
+        type: 'css',
         options: {
-          additionalData: `@import '@weni/unnnic-system/src/assets/scss/unnnic.scss';`, // Importa automaticamente o arquivo SCSS
+          additionalData: `@import '@weni/unnnic-system/src/assets/scss/unnnic.scss';`,
         },
       },
       {
-        test: /\.(png|jpe?g|gif|svg|webp|avif)$/i, // Identifica os arquivos de imagem
-        type: 'asset/resource', // Processa os arquivos como recursos
+        test: /\.(png|jpe?g|gif|svg|webp|avif)$/i,
+        type: 'asset/resource',
         generator: {
-          filename: 'assets/images/[name][hash][ext]', // Define onde salvar os arquivos no build
+          filename: 'assets/images/[name][hash][ext]',
         },
       },
     ],
@@ -77,6 +93,7 @@ export default defineConfig({
       'process.env': JSON.stringify(process.env),
       'import.meta.env': JSON.stringify({
         BASE_URL: process.env.BASE_URL || '/',
+        DEV: process.env.NODE_ENV === 'development' ? true : false,
       }),
     }),
     new VueLoaderPlugin(),
