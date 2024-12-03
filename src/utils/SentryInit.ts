dd5a23fde741c497ca858c59a841c2512466aa9f
Marcus Vinícius <marcusvinici25@gmail.com>
2024-12-03 19:14:30 -0300

    rm var sentry

--- src/utils/SentryInit.ts
+++ src/utils/SentryInit.ts
@@ -5,7 +5,7 @@ import type { createApp } from 'vue';
 export const SentryInit = ({ app }: { app: ReturnType<typeof createApp> }) => {
   if (getEnv('SENTRY_DSN')) {
     Sentry.init({
-      release: `${__APP_NAME__}@${__APP_VERSION__}`,
+      release: `name_sentry_test@$skjfhsdkhfkj2837462876487236432`,
       app,
       dsn: getEnv('SENTRY_DSN'),
       environment: getEnv('SENTRY_ENVIRONMENT'),
