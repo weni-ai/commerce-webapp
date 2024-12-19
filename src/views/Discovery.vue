8c6aa888d5e4ec96f65da8f9f53a4b003ea6aea9
Marcus Vinícius <marcusvinici25@gmail.com>
2024-12-19 19:10:16 -0300

    adjusting Discovery to support new variant via module federation

--- src/views/Discovery.vue
+++ src/views/Discovery.vue
@@ -1,5 +1,8 @@
 <template>
   <SolutionsList
+    v-if="
+      type === 'default' || (type === 'remote' && auth?.token && auth?.uuid)
+    "
     show="available"
     :isFirstLoading="
       solutionsActiveStore.all.isFirstLoading ||
@@ -15,13 +18,38 @@ import SolutionsList from '@/components/solutions/SolutionsList.vue';
 import { onMounted } from 'vue';
 import { useSolutionsActiveStore } from '@/stores/SolutionsActive';
 import { useSolutionsPassiveStore } from '@/stores/SolutionsPassive';
+import { useAuthStore } from '@/stores/Auth';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'default',
+  },
+  auth: {
+    type: Object as () => { token: string; uuid: string } | null,
+    default: null,
+  },
+});
 
 const solutionsActiveStore = useSolutionsActiveStore();
 const solutionsPassiveStore = useSolutionsPassiveStore();
 
+if (props.type === 'remote' && props.auth?.token && props.auth?.uuid) {
+  const authStore = useAuthStore();
+  authStore.setToken(props.auth.token);
+  authStore.setProjectUuid(props.auth.uuid);
+}
+
 onMounted(() => {
-  solutionsActiveStore.all.load();
-  solutionsPassiveStore.all.load();
+  if (props.type === 'remote' && props.auth?.token && props.auth?.uuid) {
+    solutionsActiveStore.all.load();
+    solutionsPassiveStore.all.load();
+  }
+
+  if (props.type === 'default') {
+    solutionsActiveStore.all.load();
+    solutionsPassiveStore.all.load();
+  }
 });
 </script>
 
