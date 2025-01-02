8aa25c606492b7929ea09220852f649cb0bc5c90
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-02 15:04:24 -0300

    adding new logic to isFirstLoading

--- src/views/Discovery.vue
+++ src/views/Discovery.vue
@@ -4,12 +4,7 @@
       type === 'default' || (type === 'remote' && auth?.token && auth?.uuid)
     "
     show="available"
-    :isFirstLoading="
-      type === 'default' ? 
-        (solutionsActiveStore.all.isFirstLoading || solutionsPassiveStore.all.isFirstLoading) 
-      : 
-        solutionsDefaultStore.all.isFirstLoading
-    "
+    :isFirstLoading="isFirstLoading"
     :activeNotifications="solutionsActiveStore"
     :passiveService="solutionsPassiveStore"
     :integrateSkills="solutionsDefaultStore"
@@ -18,7 +13,7 @@
 
 <script setup lang="ts">
 import SolutionsList from '@/components/solutions/SolutionsList.vue';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useSolutionsActiveStore } from '@/stores/SolutionsActive';
 import { useSolutionsPassiveStore } from '@/stores/SolutionsPassive';
 import { useSolutionsDefaultStore } from '@/stores/SolutionsDefault';
@@ -38,6 +33,16 @@ const props = defineProps({
 const solutionsActiveStore = useSolutionsActiveStore();
 const solutionsPassiveStore = useSolutionsPassiveStore();
 const solutionsDefaultStore = useSolutionsDefaultStore();
+
+const isFirstLoading = computed(() => {
+  const loadingDefault =
+    solutionsActiveStore.all.isFirstLoading ||
+    solutionsPassiveStore.all.isFirstLoading;
+
+  const loadingRemote = solutionsDefaultStore.all.isFirstLoading;
+
+  return props.type === 'default' ? loadingDefault : loadingRemote;
+});
 
 if (props.type === 'remote' && props.auth?.token && props.auth?.uuid) {
   const authStore = useAuthStore();
