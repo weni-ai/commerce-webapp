8f09cff5552fc2c31dcfb5f9d21b3914ecccaf83
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-06 18:30:49 -0300

    adding dynamic remote url

--- rspack.config.js
+++ rspack.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'http://localhost:3001/',
+    publicPath: process.env.PUBLIC_PATH_URL,
     clean: true,
     filename: 'assets/js/[name]-[contenthash].js',
     chunkFilename: 'assets/js/[name]-[contenthash].js',
