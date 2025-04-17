1c8f7fb3135c6d2a6b6279703cad39dfee5ec4ca
acnormun <anaclara.noronha@weni.ai>
2025-04-17 16:44:26 -0300

    Update external login route path from '/loginexternal' to '/'

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/loginexternal',
+      path: '/',
       name: 'externalLogin',
       component: {},
       beforeEnter: async (to, from, next) => {
